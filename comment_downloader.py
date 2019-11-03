4f7065591175874d2a0d70b8eb9b411d90cfc3c7
Rish Agarwal <rishubh@Rishs-MacBook-Pro.local>
2019-11-02 23:13:59 -0400

    modified download comments and added multiprocessing to it

--- comment_downloader.py
+++ comment_downloader.py
@@ -7,12 +7,13 @@ import argparse
 import lxml.html
 import io
 from lxml.cssselect import CSSSelector
+from multiprocessing import Pool
 
 YOUTUBE_COMMENTS_URL = 'https://www.youtube.com/all_comments?v={youtube_id}'
 YOUTUBE_COMMENTS_AJAX_URL = 'https://www.youtube.com/comment_ajax'
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-
+ret_cids = []
 
 def find_value(html, key, num_chars=2):
     pos_begin = html.find(key) + len(key) + num_chars
@@ -45,7 +46,7 @@ def ajax_request(session, url, params, data, retries=10, sleep=20):
             
         else:
             time.sleep(sleep)
-def test():
+def get_tokens_html():
 
     session = requests.Session()
     session.headers['User-Agent'] = USER_AGENT
@@ -60,6 +61,7 @@ def test():
     html = response.text
 
     page_token = find_value(html, 'data-token')
+  
     session_token = find_value(html, 'XSRF_TOKEN', 4)
     first_iteration = True
     while page_token:
@@ -85,99 +87,67 @@ def test():
     return token_lst, html_lst
 
 
-#def initial(token, html):
+def download_comments(token, html):
 
-#def download_rest(token, html):
-
-    
-
-def download_comments(youtube_id, sleep=1):
-    session = requests.Session() 
-    session.headers['User-Agent'] = USER_AGENT
-
-    response = session.get(YOUTUBE_COMMENTS_URL.format(youtube_id=youtube_id))
-
-    html = response.text
-
-    page_token = find_value(html, 'data-token')
-    session_token = find_value(html, 'XSRF_TOKEN', 4)
-   
+    for comment in extract_comments(html):
+        if comment['cid'] not in ret_cids:
+            ret_cids.append(comment['cid'])
+            print(comment)
+            x+=1
+            #yield comment
 
 
-    first_iteration = True
+# def main(argv):
+#     parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
+#     parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
+#     parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
+#     parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
+#     parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
 
-    ret_cids = []
+#     try:
+#         args = parser.parse_args(argv)
 
-    while page_token:
-        data = {'video_id': youtube_id,
-                'session_token': session_token}
+#         youtube_id = args.youtubeid
+#         output = args.output
+#         limit = args.limit
 
-        params = {'action_load_comments': 1,
-                  'order_by_time': True,
-                  'filter': youtube_id}
-                  
+#         if not youtube_id or not output:
+#             parser.print_usage()
+#             raise ValueError('you need to specify a Youtube ID and an output filename')
 
-        if first_iteration:
-            params['order_menu'] = True
-        else:
-            data['page_token'] = page_token
-
-        response = ajax_request(session, YOUTUBE_COMMENTS_AJAX_URL, params, data)
-       
-
-        page_token, html = response
-        
-
-        for comment in extract_comments(html):
-            if comment['cid'] not in ret_cids:
-                ret_cids.append(comment['cid'])
-                yield comment
-
-        first_iteration = False
-        time.sleep(sleep)
-
+#         print('Downloading Youtube comments for video:', youtube_id)
+#         count = 0
+#         with io.open(output, 'w', encoding='utf8') as fp:
+#             for comment in download_comments(youtube_id):
+#                 comment_json = json.dumps(comment, ensure_ascii=False)
+#                 print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
+#                 count += 1
+#                 sys.stdout.write('Downloaded %d comment(s)\r' % count)
+#                 sys.stdout.flush()
+#                 if limit and count >= limit:
+#                     break
+#         print('\nDone!')
 
 
-
-def main(argv):
-    parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
-    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
-    parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
-    parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
-    parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
-
-    try:
-        args = parser.parse_args(argv)
-
-        youtube_id = args.youtubeid
-        output = args.output
-        limit = args.limit
-
-        if not youtube_id or not output:
-            parser.print_usage()
-            raise ValueError('you need to specify a Youtube ID and an output filename')
-
-        print('Downloading Youtube comments for video:', youtube_id)
-        count = 0
-        with io.open(output, 'w', encoding='utf8') as fp:
-            for comment in download_comments(youtube_id):
-                comment_json = json.dumps(comment, ensure_ascii=False)
-                print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
-                count += 1
-                sys.stdout.write('Downloaded %d comment(s)\r' % count)
-                sys.stdout.flush()
-                if limit and count >= limit:
-                    break
-        print('\nDone!')
-
-
-    except Exception as e:
-        print('Error:', str(e))
-        sys.exit(1)
+    # except Exception as e:
+    #     print('Error:', str(e))
+    #     sys.exit(1)
 
 
 if __name__ == "__main__":
-    # test = ['-y', 'rxuVzaNne7Q', '-o', 'test.txt']
+    # test = ['-y', 'KnEpLGYS0SM', '-o', 'test.txt']
     # main(test)
-    x = test()
+
+
+    #download_comments_old('KnEpLGYS0SM')
+
+    token, html = get_tokens_html()
+    # pool = Pool(4)
+    # pool.starmap(download_comments, zip(token,html))
+    # pool.close()
+    # pool.join()
+   
+    
+   
+    
 
