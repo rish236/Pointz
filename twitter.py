4a766523b4a0cd6b124442f550da541b575472aa
Rish Agarwal <rishubh@Rishs-MacBook-Pro.local>
2019-12-03 22:04:49 -0500

    added main

--- twitter.py
+++ twitter.py
@@ -4,10 +4,9 @@ from requests_oauthlib import OAuth1
 import json
 import ast
 
-target = "theweeknd"
-source = "shaden78343862"
 
 
+def friendship(source, target)
 url = 'https://api.twitter.com/1.1/friendships/show.json?source_screen_name=%s&target_screen_name=%s'%(source, target)
 
 auth = OAuth1(config.twitter_consumer, config.twitter_consumer_secret, config.twitter_access, config.twitter_access_secret)
@@ -21,3 +20,10 @@ my_dict = ast.literal_eval(new)
 for k,v in my_dict.items():
     if k == "following":
         print(v)
+
+
+if __name__ == "__main__":
+    target = "theweeknd"
+    source = "shaden78343862"
+    friendship(source, target)
+
